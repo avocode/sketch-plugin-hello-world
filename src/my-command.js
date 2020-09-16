f07d375fa587face4fbc05d64d7426949f990e51
Jakub Žitný <jakubzitny@avocode.com>
2020-09-16 23:48:08 +0200

    count number of artboards and pages in a Sketch file

--- src/my-command.js
+++ src/my-command.js
@@ -1,3 +1,12 @@
-export default function(context) {
-  context.document.showMessage("It's alive 🙌")
+const dom = require('sketch/dom')
+const ui = require('sketch/ui')
+
+export default function() {
+  const doc = dom.getSelectedDocument()
+  const pageCount = doc.pages.length
+  const artboardCount = doc.pages.reduce((count, page) => {
+    return count + page.layers.length
+  }, 0)
+
+  ui.message(`There are ${pageCount} pages and ${artboardCount} artboards 🎨`)
 }
